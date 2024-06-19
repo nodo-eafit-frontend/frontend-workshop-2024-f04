5c243a4a126256dee6d075ef615235ca9d47edce
Marvin Cely <mardancely@gmail.com>
2024-06-19 16:55:00 -0500

    Agregar taller 3 JS fundamentals

--- 0-workshops/3-rock-paper-scissors-js/index.js
+++ 0-workshops/3-rock-paper-scissors-js/index.js
@@ -1,31 +1,83 @@
-// Variables
+// Importamos el módulo 'prompts' para obtener entradas del usuario
+import prompts from 'prompts';
 
-// Data Types
+// Importamos funciones y variables
+import { CHOICES, MESSAGES, stats } from './constants/index.js';
+import getCurrentChoice from './functions/get-current-choice.js';
+import determineWinner from './functions/determine-winner.js';
+import getComputerChoice from './functions/get-computer-choice.js';
+import { getChoicesMessage, getScoreMessage } from './functions/messages.js';
 
-// Operators
+/**
+ * Inicia el juego de Piedra, Papel o Tijera.
+ *
+ * Esta función utiliza el módulo 'prompts' para interactuar con el usuario,
+ * obtiene las elecciones del jugador y la computadora, y luego determina
+ * y anuncia el ganador.
+ *
+ * @async
+ * @function playGame
+ *
+ * @returns {Promise<void>} No retorna ningún valor, solo imprime los resultados del juego en la consola.
+ */
+const playGame = async () => {
+	// Preguntar por la opción
+	const response = await prompts({
+		type: 'select',
+		name: 'playerChoiceValue',
+		message: MESSAGES.question,
+		choices: [
+			{ title: `${CHOICES.rock.text} ${CHOICES.rock.emoji}`, value: CHOICES.rock.value },
+			{ title: `${CHOICES.paper.text} ${CHOICES.paper.emoji}`, value: CHOICES.paper.value },
+			{ title: `${CHOICES.scissors.text} ${CHOICES.scissors.emoji}`, value: CHOICES.scissors.value },
+		],
+	});
 
-// -- Unary operators
+	// Obtener elección del jugador y del computador
+	const playerChoiceValue = response.playerChoiceValue;
+	const computerChoiceValue = getComputerChoice();
 
-// -- Arithmetic operators
+	// Obtener opciones elegidas a partir de los valores
+	const playerChoice = getCurrentChoice(playerChoiceValue);
+	const computerChoice = getCurrentChoice(computerChoiceValue);
 
-// -- Relational operators
+	console.log('\x1Bc'); // Limpiar pantalla
 
-// -- Equality operators
+	// Anunciar opciones elejidas
+	const choicesMessage = getChoicesMessage(playerChoice, computerChoice);
+	console.log(choicesMessage);
 
-// -- Binary logical operators
+	const result = determineWinner(playerChoice, computerChoice);
 
-// Type Conversion & Coercion
+	return result;
+};
 
-// Conditionals
+/**
+ * Jugar el juego en ciclos hasta que el jugador pierda todas la vidas
+ */
+async function runGame() {
+	while (stats.lifes > 0) {
+		const scoreMessage = getScoreMessage(stats);
+		console.log(scoreMessage, '\n');
+		const result = await playGame();
 
-// Conditional (ternary) operator
+		console.log('\t', MESSAGES.results[result], '\n'); // Anunciar Ganador
 
-// Functions
+		if (result === 0) {
+			stats.score += stats.byDraw;
+		} else if (result === 1) {
+			stats.score += stats.byWin;
+		} else {
+			stats.lifes--;
+		}
+	}
 
-// Loops
+	const scoreMessage = getScoreMessage(stats);
+	console.log(scoreMessage, '\n');
+	console.log('\t', MESSAGES.gameOver);
 
-// Arrays
+	stats.attemps++;
+}
 
-// Objects
-
-// Spread syntax
+// Start the game
+runGame();
