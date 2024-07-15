573d1c828a6407e5391d84c7cf5b776c80bd44b8
Alejogm2022 <alejandro.gomezmeneses@gmail.com>
2024-07-15 18:10:36 -0500

    "ajustes iniciales proyecto"

--- 0-workshops/3-rock-paper-scissors-js/tests/get-computer-choice.test.js
+++
@@ -1,9 +0,0 @@
-import getComputerChoice from '../functions/get-computer-choice.js';
-
-describe('getComputerChoice', () => {
-	it('should return a random number between 0 and 2', () => {
-		const choice = getComputerChoice();
-		expect(choice).toBeGreaterThanOrEqual(0);
-		expect(choice).toBeLessThanOrEqual(2);
-	});
-});
